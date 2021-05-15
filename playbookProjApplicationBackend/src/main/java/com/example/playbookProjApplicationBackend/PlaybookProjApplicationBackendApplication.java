976425ccda84d4d7d55fdf6f74908c6fee14edd6
Temi Owoaje <temiowoaje33@hotmail.com>
2021-05-15 16:30:41 -0400

    added bootstrap spring application

---
+++ playbookProjApplicationBackend/src/main/java/com/example/playbookProjApplicationBackend/PlaybookProjApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.playbookProjApplicationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaybookProjApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaybookProjApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ playbookProjApplicationBackend/src/test/java/com/example/playbookProjApplicationBackend/PlaybookProjApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playbookProjApplicationBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaybookProjApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
