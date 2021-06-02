c92ef012c76ce8093e3b36ad3639490333b94d93
Temi Owoaje <temiowoaje33@hotmail.com>
2021-06-02 13:37:09 -0400

    cleaned up main application class

--- playbookProjApplicationBackend/src/main/java/com/example/playbookProjApplicationBackend/PlaybookProjApplicationBackendApplication.java
+++ playbookProjApplicationBackend/src/main/java/com/example/playbookProjApplicationBackend/PlaybookProjApplicationBackendApplication.java
@@ -10,6 +10,6 @@ public class PlaybookProjApplicationBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlaybookProjApplicationBackendApplication.class, args);
 	}
-	
+
 
 }
